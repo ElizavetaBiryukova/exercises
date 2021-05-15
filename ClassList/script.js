ffb2215379a40f4be9bc684707f36d2715748ec5
ElizavetaBiryukova <birliza@mail.ru>
2021-05-15 19:38:55 +0200

    Adds event listener functions

--- ClassList/script.js
+++ ClassList/script.js
@@ -1,4 +1,5 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
 // console.log(btns[0].classList.length);
 // console.log(btns[0].classList.item(0));
 
@@ -19,3 +20,29 @@ btns[0].addEventListener('click', () => {
     // }
     btns[1].classList.toggle('red');
 });
+
+//Обработчик на родителе
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName == "BUTTON") {
+        console.log('Hello');
+    }
+});
+
+//Вариант перебора новые динамические элементы не видят hello
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+        
+//     });
+// });
+
+//Вариант с методом matches
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches('button.red')) {
+        console.log('Hello');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
