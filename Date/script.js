75f40664a11c0e28fed45f53ea477870a17c708a
ElizavetaBiryukova <birliza@mail.ru>
2021-05-16 22:27:12 +0200

    Adds Date

---
+++ Date/script.js
@@ -0,0 +1,14 @@
+
+const now = new Date();
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+
+// console.log(now.getDate());
+// console.log(now.getDay());
+
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+console.log(now.getTimezoneOffset());
