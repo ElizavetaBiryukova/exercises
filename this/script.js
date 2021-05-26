191f9e5e899ac07794fd040e32ecd9890b193a77
ElizavetaBiryukova <birliza@mail.ru>
2021-05-26 22:43:12 +0200

    Adds this

---
+++ this/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+//1) Обычная функция: this = window, но если use strict - undefined
+
+// function showThis(a, b) {
+//     console.log(this);
+
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+
+// showThis(4, 5);
+
+//2)  Контекст у методов объекта - сам объект
+
+const obj = {
+    a:20,
+    b: 15,
+    sum: function() {
+        console.log(this);
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();
+
+
+//3) 
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+
+}
+
+const alex = new User('Alex', 28);
