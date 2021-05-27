b075e17b19f58020af6fb63128ef640881cb80e7
ElizavetaBiryukova <birliza@mail.ru>
2021-05-27 22:25:27 +0200

    Adds 4 point. Call, apply, bind

--- this/script.js
+++ this/script.js
@@ -1,46 +1,66 @@
 'use strict';
 
-//1) Обычная функция: this = window, но если use strict - undefined
+// //1) Обычная функция: this = window, но если use strict - undefined
 
-// function showThis(a, b) {
-//     console.log(this);
+// // function showThis(a, b) {
+// //     console.log(this);
 
-//     function sum() {
+// //     function sum() {
+// //         console.log(this);
+// //         return a + b;
+// //     }
+// //     console.log(sum());
+// // }
+
+// // showThis(4, 5);
+
+// //2)  Контекст у методов объекта - сам объект
+
+// const obj = {
+//     a:20,
+//     b: 15,
+//     sum: function() {
 //         console.log(this);
-//         return a + b;
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
 //     }
-//     console.log(sum());
+// };
+
+// obj.sum();
+
+
+// //3) this в конструкторах и классах - это новый экземпляр объекта
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
 // }
 
-// showThis(4, 5);
 
-//2)  Контекст у методов объекта - сам объект
+// const alex = new User('Alex', 28);
 
-const obj = {
-    a:20,
-    b: 15,
-    sum: function() {
-        console.log(this);
-        function shout() {
-            console.log(this);
-        }
-        shout();
-    }
-};
-
-obj.sum();
-
-
-//3) this в конструкторах и классах - это новый экземпляр объекта
-
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
+//4) Ручная привязка this: call, apply, bind
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
 }
 
+const user = {
+    name: 'John',
+};
 
-const alex = new User('Alex', 28);
+sayName.call(user, ' Smith');
+sayName.apply(user, [' Smith']);
+
+function count(num) {
+    return this*num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
