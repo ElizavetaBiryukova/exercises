3c25ad55272166da8177c6669d8b9ec5aa9d73e5
ElizavetaBiryukova <birliza@mail.ru>
2021-05-31 21:01:51 +0200

    Adds rest

--- rest/script.js
+++ rest/script.js
@@ -1,3 +1,19 @@
-const log = function(a, b, c) {
-    console.log();
+const log = function (a, b, ...rest) {
+    console.log(a, b, rest);
 };
+
+log('basic', 'rest', 'operator', 'usage');
+
+
+function calcOrDouble(number, basis) {
+    basis = basis || 2;
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
+
+function calcOrDouble(number, basis = 2 ) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
