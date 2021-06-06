d272f2d6b04313c16f42c4db5c760f58b6437882
ElizavetaBiryukova <birliza@mail.ru>
2021-06-06 15:18:27 +0200

    Adds AJAX

--- calc/js/script.js
+++ calc/js/script.js
@@ -10,4 +10,23 @@ inputRub.addEventListener('input', () => {
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
+    // request.addEventListener('readystatechange', () => {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    //     } else {
+    //         inputUsd.value = 'Что-то пошло не так';
+    //     }
+    // });
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Что-то пошло не так';
+        }
+    });
+
 });
