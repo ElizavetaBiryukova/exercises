34159237c107360f92790858710d0507af081d06
ElizavetaBiryukova <birliza@mail.ru>
2021-06-13 23:08:29 +0200

    Adds map method

--- array_methods/script.js
+++ array_methods/script.js
@@ -4,8 +4,20 @@
 
 const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
 
-const shortNames = names.filter((name) => {
+const shortNames = names.filter(name => {
     return name.length < 5;
 });
 
 console.log(shortNames);
+
+//map
+
+const answers = ['IvAn', 'AnnA', 'Hello'];
+
+// const result = answers.map(item => {
+//     return item.toLowerCase();
+// });
+
+const result = answers.map(item => item.toLowerCase());
+
+console.log(result);
