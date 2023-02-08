79dd36655e1f5e28499cb4e86a1c0e838220b8ef
Elizaveta Biryukova <birliza@mail.ru>
2023-02-08 15:13:24 +0200

    fix: add /exercises

--- array_methods/script.js
+++ array_methods/script.js
@@ -101,3 +101,28 @@ function toWeirdCase(string) {
 }
 
 console.log(toWeirdCase("string fgf"))
+
+
+
+function hexFormat(number) {
+    return number.length < 2 ? '0' + number : number
+}
+
+function rgb(r, g, b) {
+    const rgbArray = [r, g, b];
+    let hex = '';
+
+    for (let number of rgbArray) {
+        if (number < 0) {
+            number = 0
+        } else if (number > 255) {
+            number = 255
+        }
+        hex = hex + hexFormat(parseInt(number, 10).toString(16).toUpperCase())
+
+    }
+    console.log(hex)
+    return hex
+}
+
+console.log(rgb(173,255,47))
